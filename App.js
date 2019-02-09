e3c4e8cc69084e0460e035c5deaed0c7264c67e4
Trinh Son <caoson.trinh@gmail.com>
2019-02-09 14:40:29 +0200

    color and cover

--- App.js
+++ App.js
@@ -1,66 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Item, Input, Footer, FooterTab, Button, Text, Icon } from 'native-base';
+import { Container, Header, Content, Item, Input, Footer, FooterTab, Button as BaseButton, Text as BaseText, Icon } from 'native-base';
 import { Font, AppLoading } from "expo";
-import { View, Text } from "react-native";
+import { AppRegistry, View, Text, Button, Image, StyleSheet, StatusBar } from "react-native";
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 
-export default class FooterTabsExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
-
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-    });
-    this.setState({ loading: false });
-  }
-
-  render() {
-    if (this.state.loading) {
-      return <Expo.AppLoading />;
-    }
-    return (
-      <Container>
-        <Header searchBar rounded>
-          <Item>
-            <Icon ios='ios-search' android="md-search" />
-            <Input placeholder="Search" />
-          </Item>
-          <Button transparent>
-            <Text>Search</Text>
-          </Button>
-        </Header>
-
-        <Content />
-
-        <Footer>
-          <FooterTab>
-            <Button active>
-              <Icon ios='ios-search' android="md-search" style={{fontSize: 20}}/>
-              <Text>Search</Text>
-            </Button>
-            <Button>
-              <Icon ios='ios-bookmark' android="md-bookmark" style={{fontSize: 20}}/>
-              <Text>Saved</Text>
-            </Button>
-            <Button>
-              <Icon ios='ios-book' android="md-book" style={{fontSize: 20}}/>
-              <Text style={{fontSize: 9}}>Collection</Text>
-            </Button>
-            <Button>
-              <Icon ios='ios-settings' android="md-settings" style={{fontSize: 20}}/>
-              <Text>Settings</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );  
-  }
-}
+const styles = StyleSheet.create({
+  canvas: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+});
 
 class HomeScreen extends React.Component {
   render() {
@@ -104,3 +54,75 @@ const AppNavigator = createStackNavigator({
     initialRouteName: 'Home',
 });
 
+const AppContainer = createAppContainer(AppNavigator);
+
+
+export default class FooterTabsExample extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  // load font after native base
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
+    return (
+      <Container>
+        {/* hide status bar */}
+        <StatusBar hidden />
+        <Header style={{ backgroundColor: '#3B8686' }} searchBar rounded>
+          <Item>
+            <Icon ios='ios-search' android="md-search" />
+            <Input placeholder="Search" />
+          </Item>
+          <BaseButton transparent>
+            <BaseText>Search</BaseText>
+          </BaseButton>
+        </Header>
+
+        {/* <AppContainer>
+        </AppContainer> */}
+        {/* min height make content appear */}
+        <Content contentContainerStyle={{ minHeight: 300 }}>
+          <Image
+            resizeMode="contain"
+            source={require('./img/cover.png')}
+            style={styles.canvas} >
+          </Image>
+        </Content>
+
+        <Footer>
+          <FooterTab style={{ backgroundColor: '#3B8686' }}>
+            <BaseButton active style={{ backgroundColor: '#3B8686' }}>
+              <Icon ios='ios-search' android="md-search" style={{fontSize: 20}}/>
+              <BaseText>Search</BaseText>
+            </BaseButton>
+            <BaseButton>
+              <Icon ios='ios-bookmark' android="md-bookmark" style={{fontSize: 20}}/>
+              <BaseText>Saved</BaseText>
+            </BaseButton>
+            <BaseButton>
+              <Icon ios='ios-book' android="md-book" style={{fontSize: 20}}/>
+              <BaseText style={{fontSize: 9}}>Collection</BaseText>
+            </BaseButton>
+            <BaseButton>
+              <Icon ios='ios-settings' android="md-settings" style={{fontSize: 20}}/>
+              <BaseText>Settings</BaseText>
+            </BaseButton>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );  
+  }
+}
